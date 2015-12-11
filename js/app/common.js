c91581f9cddd821b1b7334a9eb9502543b75fe13
systemshock89 <system.shock89@gmail.com>
2015-12-11 15:35:02 +0300

    sync slider fix2

--- js/app/common.js
+++ js/app/common.js
@@ -89,7 +89,6 @@ $(function () {
 
         var $sync1 = $(".synced_slider1 .owl-carousel"),
             $sync2 = $(".synced_slider2 .owl-carousel"),
-            flag = false,
             duration = 300;
 
         $sync1
@@ -98,25 +97,28 @@ $(function () {
                 rewind:true,
                 margin: 10,
                 dots: false,
-                //autoplay: true,
-                autoplayTimeout: 12000,
+                autoplay: true,
+                autoplayTimeout: 2000,
                 autoplayHoverPause: true,
                 touchDrag: false,
                 mouseDrag : false
             })
             .on('changed.owl.carousel', function (e) {
-                if (!flag) {
-                    flag = true;
-                    $sync2.trigger('to.owl.carousel', [e.item.index, duration, true]);
-                    flag = false;
+                var syncedPosition = syncPosition(e.item.index);
+
+                if ( syncedPosition != "stayStill" ) {
+                    $sync2.trigger('to.owl.carousel', [syncedPosition, duration, true]);
                 }
             });
 
         $sync2
+            .on('initialized.owl.carousel', function() { //must go before owl carousel initialization
+                addClassCurrent( 0 );
+            })
             .owlCarousel({
                 items: 4,
                 rewind:true,
-                center: true,
+                //center: true,
                 margin: 18,
                 nav: true,
                 navText:  [
@@ -127,15 +129,48 @@ $(function () {
             })
             .on('click', '.owl-item', function () {
                 $sync1.trigger('to.owl.carousel', [$(this).index(), duration, true]);
-
-            })
-            .on('changed.owl.carousel', function (e) {
-                if (!flag) {
-                    flag = true;
-                    $sync1.trigger('to.owl.carousel', [e.item.index, duration, true]);
-                    flag = false;
-                }
             });
+
+        //adds 'current' class to the thumbnail
+        function addClassCurrent( index ) {
+            $sync2
+                .find(".owl-item")
+                .removeClass("current")
+                .eq( index ).addClass("current");
+        }
+
+        //syncs positions. argument 'index' represents absolute position of the element
+        function syncPosition( index ) {
+
+            //PART 1 (adds 'current' class to thumbnail)
+            addClassCurrent( index );
+
+
+            //PART 2 (counts position)
+
+            var itemsNo = $sync2.find(".owl-item").length; //total items
+            var visibleItemsNo = $sync2.find(".owl-item.active").length; //visible items
+
+            //if all items are visible
+            if (itemsNo === visibleItemsNo) {
+                return "stayStill";
+            }
+
+            //relative index (if 4 elements are visible and the 2nd of them has class 'current', returns index = 1)
+            var visibleCurrentIndex = $sync2.find(".owl-item.active").index( $sync2.find(".owl-item.current") );
+
+            //if it's first visible element and if there is hidden element before it
+            if (visibleCurrentIndex == 0 && index != 0) {
+                return index - 1;
+            }
+
+            //if it's last visible element and if there is hidden element after it
+            if (visibleCurrentIndex == (visibleItemsNo - 1) && index != (itemsNo - 1)) {
+                return index - visibleItemsNo + 2;
+            }
+
+            return "stayStill";
+        }
 
     }
     /* /SYNCED Owl Slider */
